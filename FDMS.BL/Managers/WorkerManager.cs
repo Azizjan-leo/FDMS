111c5db8cfe2f57633993e18b514ed2645f60a08
azizjan.ayupov <azizjan.ayupov@gmail.com>
2024-06-11 23:10:33 +0600

    solution structure

---
+++ FDMS.BL/Managers/WorkerManager.cs
@@ -0,0 +1,27 @@
+﻿using FDMS.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FDMS.Model.Entities
+{
+    public class WorkerManager
+    {
+        // Mock api data
+        public static ObservableCollection<Worker> Workers = new ObservableCollection<Worker>()
+        {
+            new Worker(1, "Скворцов Александр", "alex@gmail.com"),
+            new Worker(2, "Вилисов Михаил Юрьевич", "mx123@gmail.com"),
+            new Worker(3, "Гаденко Владислав Владимирович", "glavgad@gmail.com"),
+            new Worker(4, "Климов Петр Михайлович", "tsar777@gmail.com"),
+            new Worker(5, "Андропов Алексей Андреевич", "alexgeometry@gmail.com"),
+            new Worker(6, "Бобровский Дмитрий Львович", "notkurwajustbober@gmail.com"),
+            new Worker(7, "Репин Евгений Федерович", "repagen@gmail.com"),
+            new Worker(8, "Седокина Алина", "salina086@gmail.com"),
+        };
+        public WorkerManager() { }
+    }
+}
--- FDMS.Model/Entities/Worker.cs
+++ FDMS.Model/Entities/Worker.cs
@@ -11,5 +11,12 @@ namespace FDMS.UI.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+
+        public Worker(int id, string name, string email)
+        {
+            Id=id;
+            Name=name;
+            Email=email;
+        }
     }
 }
